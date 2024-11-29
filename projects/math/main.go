db8ac33d5c2c0aba45b4a6001eca0d71e7c679d9
Ruslan <mr.krasava34@mail.ru>
2024-11-30 00:53:32 +0300

    OMG it Works!

--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+var k, p, v float64
+
+func M() float64 {
+	// Масса m = p * v
+	return p * v
+}
+
+func W() float64 {
+	// Циклическая частота w = sqrt(k / m)
+	return math.Sqrt(k / M())
+}
+
+func T() float64 {
+	// Период колебаний t = 6 / w
+	return 6 / W()
+}
 func main() {
-	fmt.Println("Hello, world!!!")
+	fmt.Print("Введите значение k (жесткость пружины): ")
+	fmt.Scan(&k)
+
+	fmt.Print("Введите значение p (плотность): ")
+	fmt.Scan(&p)
+
+	fmt.Print("Введите значение v (объем): ")
+	fmt.Scan(&v)
+
+	fmt.Println("Период колебаний t =", T())
 }
